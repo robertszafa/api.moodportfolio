2abbbf77dfb28f4592d5be9e6cce2ba767c36192
Robert Szafarczyk <robertszafa@gmail.com>
2019-04-27 13:26:18 +0100

    added DELETE photo endpoint

--- resources/PhotoUri.py
+++ resources/PhotoUri.py
@@ -17,3 +17,30 @@ class PhotoUri(Resource):
         photo_uri = _get_photo_uri(photo_id)
 
         return jsonify({'success': True, 'error': '', 'photoUri': photo_uri})
+
+    def delete(self):
+        try:
+            user_id = _authenticate_user(request)
+            photo_id = request.headers.get('PhotoId')
+        except Exception as err:
+            return jsonify({'success': False, 'error': 'incorrectOrExpiredAuthToken'})
+        
+        print('deleting ', photo_id)
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute("DELETE FROM Photo WHERE photoID=%s AND userID=%s", (photo_id, user_id))
+            mysql.connection.commit()
+            cur.close()
+        except Exception as err:
+            print(err)
+            return jsonify({'success': False, 'error': 'databaseError'})
+        
+        photo_path = f'photos/{user_id}/{photo_id}.txt'
+        try:
+            if os.path.exists(photo_path):
+                os.remove(photo_path)
+        except Exception as err:
+            print(err)
+            return jsonify({'success': False, 'error': 'osError'})
+ 
+        return jsonify({'success': True, 'error': ''})
