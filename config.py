a5aa087cd05ce6cf27714b430a93a2da650f2290
Robert Szafarczyk <robertszafa@gmail.com>
2019-05-03 08:41:56 +0100

    new config

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ from flask import Flask, Blueprint
 from flask_mysqldb import MySQL
 from flask_restful import Api
 from flask_cors import CORS
-from mailjet_rest import Client
 
 
 
@@ -21,11 +20,6 @@ app.config['MYSQL_DB'] = 'moodportfolio'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
 mysql = MySQL(app)
-
-# EMAIL
-api_key = '6d3e2eff8e6c4cb73ef8f7e0dfcfbe2f'
-api_secret = 'a256c5bb0a86d1e99f5d18678e4649a9'
-mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
